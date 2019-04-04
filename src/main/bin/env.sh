aea4ce813f326ad388e8596a41bcd3cdda36a614
peachyy <hixstao@gmail.com>
2019-04-04 14:31:38 +0800

    fix bug

--- src/main/bin/env.sh
+++ src/main/bin/env.sh
@@ -4,6 +4,6 @@ BUILD_ID=5555555555buildid
 LOG_HOME=/var/logs/sdmq/
 LOG_PATH=$LOG_HOME
 export JAVA_HOME=/usr/local/jdk
-JAVA_OPTS=' -server  -Xms256m -Xmx256m -XX:PermSize=64m -XX:MaxPermSize=128m  -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./heap -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true  -Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=127.0.0.1 -Dcom.sun.management.jmxremote.port=6598 -Dcom.sun.management.jmxremote.authenticate=false
+JAVA_OPTS=' -server  -Xms256m -Xmx256m  -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./heap -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true  -Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=127.0.0.1 -Dcom.sun.management.jmxremote.port=6598 -Dcom.sun.management.jmxremote.authenticate=false
  -Dcom.sun.management.jmxremote.ssl=false'
 #JAVA_OPTS=' -server  -Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=219.239.88.245 -Dcom.sun.management.jmxremote.port=6598 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false'
