18943e0f6c8751ddfec7948c6c4ffe3d5352bcd3
peachyy <hixstao@gmail.com>
2019-04-04 14:20:59 +0800

    fix bug

--- src/main/bin/env.sh
+++ src/main/bin/env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BUILD_ID=5555555555buildid
-LOG_HOME=/var/jikxiu/delay-queue/logs
+LOG_HOME=/var/logs/sdmq/
 LOG_PATH=$LOG_HOME
 export JAVA_HOME=/usr/local/jdk
 JAVA_OPTS=' -server  -Xms256m -Xmx256m -XX:PermSize=64m -XX:MaxPermSize=128m  -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./heap -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true  -Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=127.0.0.1 -Dcom.sun.management.jmxremote.port=6598 -Dcom.sun.management.jmxremote.authenticate=false
