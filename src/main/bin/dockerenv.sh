4746487a5db8ac9debbc6d174a49acd5dd23d5d8
peachyy <hixstao@gmail.com>
2021-11-25 15:15:17 +0800

    k8s add

---
+++ src/main/bin/dockerenv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BUILD_ID=5555555555buildid
+LOG_HOME=/var/logs/sdmq/
+LOG_PATH=$LOG_HOME
+#export JAVA_HOME=/usr/local/jdk
+JAVA_OPTS=' -server  -Djava.security.egd=file:/dev/./urandom -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMPercentage=80.0  -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./heap -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true  -Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=127.0.0.1 -Dcom.sun.management.jmxremote.port=6598 -Dcom.sun.management.jmxremote.authenticate=false
+ -Dcom.sun.management.jmxremote.ssl=false'
+#JAVA_OPTS=' -server  -Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=219.239.88.245 -Dcom.sun.management.jmxremote.port=6598 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false'
